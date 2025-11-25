0104807e50919005a062ec5b7887f516dd2718b5
14247055 <14247055@205-22.Fortis.local>
2025-11-25 11:51:27 +0100

    Start

---
+++ src/kaserne/Kaserne.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kaserne;
+
+/**
+ *
+ * @author 14247055
+ */
+public class Kaserne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
